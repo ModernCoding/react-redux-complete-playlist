e0716d69a6a5dbe226854feb8ec4626f149bf588
Modern Coding <cormorant.programming@gmail.com>
2019-10-25 19:10:26 +0700

    Conditional output

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -21,21 +21,19 @@ const Ninjas = ({ninjas}) => {
   return (
     <div className="ninja-list">
       { 
-        ninjas.map(ninja => {
+        ninjas
+          .filter(ninja => ninja.age > 20)
+          .map(ninja => {
           
-          return ninja.age > 20 ?
-            
-            (
+            return (
               <div className="ninja" key={ninja.id}>
                 <div>Name: { ninja.name }</div>
                 <div>Age: { ninja.age }</div>
                 <div>Belt: { ninja.belt }</div>
               </div>
-            ) :
+            );
 
-            null;
-            
-        })
+          })
       }
     </div>
   );
