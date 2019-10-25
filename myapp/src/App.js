b4d6512aed723ca697b7cca6191c537884961fd6
Modern Coding <cormorant.programming@gmail.com>
2019-10-25 17:06:31 +0700

    Outputting lists

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,12 +3,19 @@ import Ninjas from './Ninjas';
 
 
 function App() {
+  const state = {
+    ninjas: [
+      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
+      { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
+      { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
+    ]
+  }
+  
   return (
     <div className="App">
       <h1>My first React app!</h1>
       <p>Welcome :)</p>
-      <Ninjas name="Yoshi" age="20" belt="green" />
-      <Ninjas name="Ryu" age="30" belt="black" />
+      <Ninjas ninjas={state.ninjas}/>
     </div>
   );
 }
