808271874148c4eee3e8d60298d31df75e70b378
Modern Coding <cormorant.programming@gmail.com>
2019-10-25 18:44:43 +0700

    Stateless Components

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Ninjas from './Ninjas';
 
 
-function App() {
-  const state = {
+class App extends Component{
+  state = {
     ninjas: [
       { name: 'Ryu', age: 30, belt: 'black', id: 1 },
       { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
       { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
     ]
   }
-  
-  return (
-    <div className="App">
-      <h1>My first React app!</h1>
-      <p>Welcome :)</p>
-      <Ninjas ninjas={state.ninjas}/>
-    </div>
-  );
+
+  render() {
+    return (
+      <div className="App">
+        <h1>My first React app!</h1>
+        <p>Welcome :)</p>
+        <Ninjas ninjas={this.state.ninjas}/>
+      </div>
+    );
+  }
 }
 
 export default App;
