4682d66b74c311dc68c37e82297b1428504ebb98
Modern Coding <cormorant.programming@gmail.com>
2019-10-27 18:36:03 +0700

    passing functions as props

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
 
-
-class App extends Component{
+class App extends Component {
   state = {
     ninjas: [
       { name: 'Ryu', age: 30, belt: 'black', id: 1 },
@@ -11,11 +11,21 @@ class App extends Component{
     ]
   }
 
+  addNinja = ninja => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja];
+
+    this.setState({
+      ninjas: ninjas
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>My first React app!</h1>
+        <h1>My first React app</h1>
         <Ninjas ninjas={this.state.ninjas}/>
+        <AddNinja addNinja={this.addNinja} />
       </div>
     );
   }
