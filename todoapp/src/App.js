b6a1df266b2be6351fe6ace3706cb2a651a0fb11
Cormorant Programming <cormorant.programming@gmail.com>
2019-10-30 17:37:45 +0700

    Part 2

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Todos from './Todos';
+import AddTodo from './AddTodo';
 
 
 class App extends Component {
@@ -8,6 +9,15 @@ class App extends Component {
       {id: 1, content: 'buy some milk'},
       {id: 2, content: 'play mario kart'}
     ]
+  }
+
+  addTodo = todo => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    
+    this.setState({
+      todos
+    });
   }
 
   deleteTodo = id => {
@@ -23,6 +33,7 @@ class App extends Component {
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo} />
       </div>
     );
   }
