92248a07a33537923571a12ab540eb13f428f36a
Cormorant Programming <cormorant.programming@gmail.com>
2019-11-04 08:45:10 +0700

    Route parameters

--- poketimes/src/App.js
+++ poketimes/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import Navbar from './components/Navbar';
-import { Route, BrowserRouter } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-
+import Navbar from './components/Navbar'
+import { Route, BrowserRouter } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Post from './components/Post'
 
 class App extends Component {
   render() {
@@ -15,6 +15,7 @@ class App extends Component {
           <Route exact path='/' component={Home}/>
           <Route path='/about' component={About} />
           <Route path='/contact' component={Contact} />
+          <Route path='/:post_id' component={Post} />
         </div>
       </BrowserRouter>
     );
--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 class Home extends Component {
   state = {
     posts: []
   }
-
   componentDidMount(){
     axios.get('https://jsonplaceholder.typicode.com/posts/')
       .then(res => {
@@ -15,7 +15,6 @@ class Home extends Component {
         });
       })
   }
-  
   render(){
     const { posts } = this.state
     const postList = posts.length ? (
@@ -23,7 +22,9 @@ class Home extends Component {
         return (
           <div className="post card" key={post.id}>
             <div className="card-content">
-              <span className="card-title">{post.title}</span>
+              <Link to={'/' + post.id}>
+                <span className="card-title red-text">{post.title}</span>
+              </Link>
               <p>{post.body}</p>
             </div>
           </div>
